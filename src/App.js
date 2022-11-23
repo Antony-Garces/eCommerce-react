82460be98e41e7346c1e9e91426382983f743a23
Antony Garces <angarpas382@hotmail.com>
2022-11-22 21:49:15 -0500

    the Navbar was created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 import Product from './components/Product.js';
 
 function App() {
   return (
     <div className="App">
-     <Product/>
+    <Navbar/>
+    <Product/>
+
 
     </div>
   );
